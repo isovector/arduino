9304cf9af3832a5ac8c0829435e62d9c75b3c836
Sandy Maguire <sandy@sandymaguire.me>
2019-06-11 19:25:36 -0400

    button to switch modes

--- mood-lighting.c
+++ mood-lighting.c
@@ -1,11 +1,12 @@
 #include <FastLED.h>
 
+#define POT_PIN 0
 #define LED_PIN   7
 #define NUM_LEDS  60
-#define INTENSITY 64
+#define INTENSITY 48
+#define SWITCH_PIN 10
 #define THRESHOLD (INTENSITY * 2)
-#define PROGRAMS 5
-#define SPEED 10
+#define PROGRAMS 6
 
 typedef void (*StateFn)();
 
@@ -21,18 +22,29 @@ void setup() {
   programs[2] = &goOut;
   programs[3] = &goIn;
   programs[4] = &goRandom;
+  programs[5] = &goCrazy;
 }
 
 
+float speed = 10;
+int brightness = 1024;
 int z = 0;
 int p = 0;
 bool needs_new_state = false;
+int buttonPresses = 0;
+
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 50;
+int buttonState = LOW;
+int lastButtonState = LOW;
+
 
 
 void setColor(CRGB* rgb) {
-  int r = cos(z * 0.02) * INTENSITY + INTENSITY;
-  int g = sin(z * 0.03  + 10) * INTENSITY + INTENSITY;
-  int b = sin(z * 0.05) * INTENSITY + INTENSITY;
+  int intensity = (int)(INTENSITY * ((float)brightness / 1024));
+  int r = cos(z * 0.02) * intensity + intensity;
+  int g = sin(z * 0.03  + 10) * intensity + intensity;
+  int b = sin(z * 0.05) * intensity + intensity;
   float div = 1;
 
   if (r + g + b > THRESHOLD) {
@@ -49,13 +61,12 @@ void setup_state(void (*func)()) {
   }
 }
 
-void next_program(int time, bool change) {
-  delay(time * SPEED);
+void next_program(float time, bool change) {
+  delay((int)(time * speed));
 
   if (change) {
     needs_new_state = true;
-    p += 17;
-    p %= PROGRAMS;
+    p = rand() % PROGRAMS;
   }
 }
 
@@ -109,12 +120,15 @@ void goIn() {
       i = 0;
   });
 
-  setColor(&leds[i]);
-  setColor(&leds[NUM_LEDS - i - 1]);
+  if (i % 2 == 0) {
+    setColor(&leds[i / 2]);
+  } else {
+    setColor(&leds[NUM_LEDS - 1 - (i - 1) / 2]);
+  }
   FastLED.show();
   i += 1;
 
-  next_program(50, i > 30);
+  next_program(25, i > 60);
 }
 
 void goRandom() {
@@ -128,12 +142,67 @@ void goRandom() {
   FastLED.show();
   i += 1;
 
-  next_program(50, i > 59);
+  next_program(25, i > 59);
+}
+
+void goCrazy() {
+  static int i = 0;
+
+  setup_state([&i]() {
+      i = 0;
+  });
+
+  z += 10;
+
+  setColor(&leds[rand() % NUM_LEDS]);
+  FastLED.show();
+  i += 1;
+
+  next_program(25, i > 59);
+}
+
+void handleButton() {
+  int reading = digitalRead(SWITCH_PIN);
+
+   if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == HIGH) {
+        buttonPresses++;
+      }
+    }
+  }
+
+  lastButtonState = reading;
+}
+
+void handleDial() {
+  int dial = analogRead(POT_PIN);
+  if (dial < 512) {
+    brightness = dial * 2;
+    speed = 10;
+  } else {
+    speed = 1 + (1024 - dial) / 512;
+  }
 }
 
 void loop() {
-  z++;
-  programs[p]();
+  handleButton();
+  handleDial();
+
+  switch (buttonPresses % 3) {
+    case 0:
+      z++;
+    case 1:
+      programs[p]();
+    case 2:
+      break;
+  }
 }
 
 
